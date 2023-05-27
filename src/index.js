acc16cccc01b13dbccc43ea0009e07ee829ce7a0
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-05-28 01:07:45 +0300

    Update formatting

--- src/index.js
+++ src/index.js
@@ -65,9 +65,10 @@ function renderBreedsSelect(breeds) {
 
 //Функція, що генерує розмітку опису обраної породи кота (картинка та текст)
 function renderBreedDesc(breed) {
-  const markupPicture = `<img class="cat-picture" width=400 src="${breed.url}" alt="${breed.id}">`;
-  const markupDescript = `<h1 class="cat-info-desc-title">${breed.breeds[0].name}</h2><p class="cat-info-desc-desc
-  ">${breed.breeds[0].description}</p><p class="cat-info-desc-temp"><b>Temperament:</b> ${breed.breeds[0].temperament}</p>`;
+  const markupPicture = `<img class="cat-picture" src="${breed.url}" alt="${breed.id}">`;
+  const markupDescript = `<h2 class="cat-info-desc-title">${breed.breeds[0].name}</h2>
+    <p class="cat-info-desc-desc">${breed.breeds[0].description}</p>
+    <p class="cat-info-desc-temp"><b>Temperament:</b> ${breed.breeds[0].temperament}</p>`;
   divPictEl.insertAdjacentHTML('beforeend', markupPicture);
   divDescEl.insertAdjacentHTML('beforeend', markupDescript);
 }
