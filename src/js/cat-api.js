9c00e6b5cd73a5f354abcb20ff8ff5aa98bfb167
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-05-28 01:06:51 +0300

    Add constances

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,24 +1,23 @@
+const url1 = 'https://api.thecatapi.com/v1/breeds';
+const url2 = 'https://api.thecatapi.com/v1/images';
+
 //Унікальний ключ доступу до Cat API
 const KEY =
   'live_HEs7npt4enTv8IppoFAzotzjElNW9aw61wQB5T2Fw18DPSakhIju9elgFzOgYqmc';
 
 //Функція, що фетчить список усіх порід котів
 const fetchBreeds = () => {
-  return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${KEY}`).then(
-    response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+  return fetch(`${url1}?api_key=${KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
     }
-  );
+    return response.json();
+  });
 };
 
 //Функція, що фетчить опис конкретної породи кота по breedId
 const fetchCatByBreed = breedId => {
-  return fetch(
-    `https://api.thecatapi.com/v1/images/${breedId}?api_key=${KEY}`
-  ).then(response => {
+  return fetch(`${url2}/${breedId}?api_key=${KEY}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
